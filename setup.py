6dac18f1e638d9e6e28284594321e50a9baa8585
unknown <harshsinghdps3@gmail.com>
2025-05-04 12:17:56 +0530

    setup and requirements

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
-from setuptools import find_packages , setup
+# Import necessary functions from setuptools for package setup and discovery
+from setuptools import find_packages, setup
 
+# Define a function to parse the requirements.txt file
 def parse_requirements():
-    with open("requirements.txt","r") as file:
-        return[line.strip() for line in file if line.strip() and not line.startswith("-e .")]
+    with open("requirements.txt", "r") as file:
+        return [line.strip() for line in file if line.strip() and not line.startswith("-e .")]
 
-
+# Configure the package metadata and dependencies using setup()
 setup(
-
-name="mlproject1",
-version="0.0.1",
-author="harsh",
-author_email="harshinghdps3@gmail.com",
-packages=find_packages(),
-install_requires=parse_requirements()
-
-
-
+    
+    name="mlproject1",
+    version="0.0.1",
+    author="harsh",
+    author_email="harshinghdps3@gmail.com",
+    packages=find_packages(),
+    install_requires=parse_requirements()
 )
