d9e938f1346fc3fd9a2b84884bd8251692558ff0
unknown <harshsinghdps3@gmail.com>
2025-05-06 10:28:39 +0530

    mlps

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 import sys
 import logging
-import logger
+from src.logger import logger
 
 #custom exception
 class CustomException(Exception):
@@ -23,6 +23,6 @@ if __name__ == "__main__":
     try:
             a=1/0
     except Exception as e:
-                logger.logger.info("divided by zero")
+                logger.info("divided by zero")
                 raise CustomException(e, sys)
     
