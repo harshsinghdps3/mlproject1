b75b7d3ed78051fc9e94f70ef7e7a81062025276
unknown <harshsinghdps3@gmail.com>
2025-05-08 09:45:25 +0530

    mlps

--- src/utils.py
+++ src/utils.py
@@ -26,12 +26,21 @@ def save_object(file_path, obj):
     logging.info(f"Object saved at {file_path}")
 
 
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
       try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
             
 
         
